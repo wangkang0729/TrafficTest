5e11e97565591d96ddedf835a1d5367810262e20
soplist <sopcase@yahoo.com>
2017-04-28 14:34:16 +0800

    试题类型管理,涉及类型xml配置文件的加载,删除和增加

---
+++ Model/QuestionClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class QuestionClass
+    {
+        private int _classID;
+        private string _className;
+        private bool _isReferences;
+
+        public int classID
+        {
+            set { _classID = value; }
+            get { return _classID; }
+        }
+
+        public string className
+        {
+            set { _className = value; }
+            get { return _className; }
+        }
+
+        public bool isReferences
+        {
+            set { _isReferences = value; }
+            get { return _isReferences; }
+        }
+    }
+}
---
+++ Tool/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool
+{
+    public class Config
+    {
+        public static string GLOBAL_QUESTION_CLASSES_XML_FILE_PATH = "question_classes\\question_classes.xml";
+    }
+}
